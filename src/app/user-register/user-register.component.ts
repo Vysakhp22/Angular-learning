b63848a5d922772c1765fe50dd3489d98861cef9
Vysakh P <vysakhp17@gmail.com>
2023-09-27 08:35:07 +0530

    Validation added for register form

--- src/app/user-register/user-register.component.ts
+++ src/app/user-register/user-register.component.ts
@@ -16,10 +16,16 @@ export class UserRegisterComponent implements OnInit {
     mobile: [, [Validators.required, Validators.pattern(this.configData.mobileRegex)]],
     dob: ['', Validators.required],
     address: ['', Validators.required],
-    age: ['', Validators.required] 
+    age: ['', Validators.required]
   });
   constructor(private fb: FormBuilder,
-              private configData: Configdata) { }
+    private configData: Configdata) { }
+
+  //get method return all the form controls
+  public get userRegisterFormConrtols() {
+    return this.userRegisterForm?.controls;
+  }
+
 
 
   ngOnInit() {
@@ -27,21 +33,24 @@ export class UserRegisterComponent implements OnInit {
   }
 
   public registerFormSubmission() {
-    console.log(this.userRegisterForm.value);
+    console.log(this.userRegisterForm?.value);
+    localStorage.setItem('user-data', JSON.stringify(this.userRegisterForm?.value));
   }
 
   public ageCalculator() {
-    let dob: any = this.userRegisterForm.get('dob')?.value;
+    let dob: any = this.userRegisterForm.get('dob')?.value; //getting dob 
     if (!dob) return;
-    dob = new Date(dob)
-    const currentDate = new Date();
+    //the type of dob is string getFullYear(), getMonth(), getDate() these funtions works only for Date type
+    //so need to convert it to date
+    dob = new Date(dob);
+    const currentDate = new Date(); //getting current date
     let age: number = 0;
     age = currentDate.getFullYear() - dob.getFullYear();
     const monthDiff = currentDate.getMonth() - dob.getMonth();
     if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < dob.getDate())) {
       age--;
     }
-    this.userRegisterForm.controls.age.setValue(String(age));
+    this.userRegisterForm.controls.age.setValue(String(age)); //assiging value to the form control, type of age is date, from control accept only string value so need to convert it to string
   }
 
 }
