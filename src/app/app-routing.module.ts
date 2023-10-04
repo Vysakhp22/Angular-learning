5207979b340164e4af0a5724f4d4f1091730a310
Vysakh P <vysakhp17@gmail.com>
2023-10-04 22:15:19 +0530

    Routing protected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { UserRegisterComponent } from './user-register/user-register.component';
 import { HomeComponent } from './Home/Home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { AuthGuard } from './auth/auth-guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'HomeComponent', pathMatch: 'full' },
+  // { path: '', redirectTo: 'HomeComponent', pathMatch: 'full' },
   {
     path: '',
     component: HomeComponent,
     children: [
       { path: 'login', component: LoginComponent },
-      { path: 'register', component: UserRegisterComponent }
+      { path: 'register', component: UserRegisterComponent },
     ]
-  }
+  },
+  { path: 'settings', canActivate: [AuthGuard], component: SettingsComponent }
 ];
 
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,7 @@ export class LoginComponent implements OnInit {
     password: new FormControl('', Validators.required)
   });
 
-  constructor(private snackBar: MatSnackBar, private authService: AuthService) { }
+  constructor(private snackBar: MatSnackBar, private authService: AuthService, private router: Router) { }
   public user: any;
   public adminData: any;
   public get loginFormControls() {
@@ -34,9 +35,11 @@ export class LoginComponent implements OnInit {
     // this.authService.adminDatacheck().then(v => d = v);
 
     if (this.adminData?.userName === this.loginForm?.value?.userName && this.adminData?.password === this.loginForm?.value?.password) {
-      this.openSnackBar('success');
+      this.authService.loggedIn();
+      this.router.navigate(['/settings']);
     } else if (this.user?.email === this.loginForm?.value?.userName && this.user?.password === this.loginForm?.value?.password) {
-      this.openSnackBar('user');
+      this.authService.loggedIn();
+      this.router.navigate(['/settings']);
     }
   }
   private openSnackBar(message: string, action?: string) {
